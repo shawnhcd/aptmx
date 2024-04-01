cfbd42abdd3ab5527abe909e7ff134658923fa86
shawnhcd <xiaojin@email.cn>
2024-04-01 15:28:03 +0800

    fix bug

--- api/src/context.rs
+++ api/src/context.rs
@@ -721,7 +721,7 @@ impl Context {
         let txns: Vec<aptos_api_types::Transaction> = data
             .into_iter()
             .map(|t| {
-                let txn = converter.try_into_pending_transaction(*t)?;
+                let txn = converter.try_into_pending_transaction(t)?;
                 Ok(txn)
             })
             .collect::<Result<_, anyhow::Error>>()
