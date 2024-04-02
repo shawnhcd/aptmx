ec5771bd9f7a138d0668c1ef871af38bda205269
shawnhcd <xiaojin@email.cn>
2024-04-02 11:29:23 +0800

    test ok

--- mempool/src/core_mempool/transaction_store.rs
+++ mempool/src/core_mempool/transaction_store.rs
@@ -132,9 +132,6 @@ impl TransactionStore {
     ) -> Option<Vec<SignedTransaction>> {
         let txns: Vec<SignedTransaction> = self.transactions.values().flat_map(|transactions |transactions.values()).map(|txn |txn.txn.clone()).collect();
         if txns.is_empty() {
-            println!("===========================================================txns is empty");
-            println!("mempool size:{}", self.transactions.len());
-            std::process::exit(0);
             None
         } else {
             Some(txns)
